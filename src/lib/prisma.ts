8ada95c0966509fe046297104a7fdb2789edbad0
amarjeeettt <amarjeet060730@gmail.com>
2024-09-29 22:40:54 +0800

    Set up CI/CD

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,4 +2,8 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+if (process.env.NODE_ENV === "production") {
+  prisma.$connect();
+}
+
 export default prisma;
